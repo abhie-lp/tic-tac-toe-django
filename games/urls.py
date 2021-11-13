aad1dc26a03eca08a8e6894be77f10475ce3dd4a
Abhishek Tiwari <abhie.lp@gmail.com>
2021-11-13 13:17:32 +0530

    Add view to make a player move

--- games/urls.py
+++ games/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import change_symbol
+from .views import change_symbol, make_a_move_view
 
 urlpatterns = [
+    path('make-move/', make_a_move_view, name="make_move"),
     path("change-symbol/", change_symbol, name="change_symbol"),
 ]
--- games/views.py
+++ games/views.py
@@ -1,6 +1,10 @@
+from operator import itemgetter
+
 from django.http import HttpResponse
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
+from django.views.decorators.csrf import csrf_exempt
+from django.views.decorators.http import require_POST
 
 
 @login_required()
@@ -18,3 +22,21 @@ def change_symbol(request):
 @login_required()
 def game_page(request):
     return render(request, "games/game.html")
+
+
+@csrf_exempt
+@require_POST
+@login_required()
+def make_a_move_view(request):
+    game = request.user.game
+    if game.symbol == "X":
+        move = 1
+    else:
+        move = 2
+
+    row, col = (int(x) for x in itemgetter("row", "col")(request.POST))
+    game.board[row][col] = move
+    game.save()
+    return HttpResponse(
+        f"<button class='square white'>{game.symbol}</button>"
+    )
