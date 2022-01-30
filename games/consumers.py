91cd4876dc3c9632bc47b100899f322d7dfaba65
Abhishek Tiwari <abhie.lp@gmail.com>
2022-01-30 14:54:04 +0530

    Change user symbol

--- games/consumers.py
+++ games/consumers.py
@@ -1,21 +1,26 @@
+from typing import Optional
+
 from asgiref.sync import async_to_sync
 from channels.generic.websocket import JsonWebsocketConsumer
 
+from accounts.models import CustomUser
 from .models import GameP2P
 
 
 class GameP2PConsumer(JsonWebsocketConsumer):
     def __init__(self, *args, **kwargs):
         super(GameP2PConsumer, self).__init__(*args, capacity=2, **kwargs)
-        self.game = None
-        self.room_group_name = None
+        self.game: Optional[GameP2P] = None
+        self.room_group_name: Optional[str] = None
+        self.user: Optional[CustomUser] = None
 
     def connect(self):
-        kwargs = self.scope['url_route']['kwargs']
         if not self.scope['user'].is_authenticated:
             self.close()
             return
-        self.game = GameP2P.game.get_game(self.scope['user'].id, kwargs['player2_id'])
+        self.user = self.scope['user']
+        self.game = GameP2P.game.get_game(self.user.id,
+                                          self.scope['url_route']['kwargs']['player2_id'])
         self.room_group_name = f'{self.game.id}{self.game.player1_id}{self.game.player2_id}'
         self.accept()
         async_to_sync(self.channel_layer.group_add)(
@@ -26,8 +31,8 @@ class GameP2PConsumer(JsonWebsocketConsumer):
             self.room_group_name,
             {
                 'type': 'player.join',
-                'message': f'{self.scope["user"].username} joined the game',
-                'user': self.scope['user'].username
+                'message': f'{self.user.username} joined the game',
+                'user': self.user.username
             }
         )
 
@@ -40,17 +45,20 @@ class GameP2PConsumer(JsonWebsocketConsumer):
             self.room_group_name,
             {
                 'type': 'player.left',
-                'message': f'{self.scope["user"].username} has left',
-                'user': self.scope['user'].username
+                'message': f'{self.user.username} has left',
+                'user': self.user.username
             }
         )
         self.close()
 
     def receive_json(self, content, **kwargs):
+        action = content['type']
+        if action == 'change.symbol':
+            content.update(self.handle_change_symbol())
         async_to_sync(self.channel_layer.group_send)(
             self.room_group_name,
             {
-                **content
+                **content,
             }
         )
 
@@ -64,4 +72,12 @@ class GameP2PConsumer(JsonWebsocketConsumer):
         self.send_json(event)
 
     def play_game(self, event):
+        self.send_json(event)
+
+    def handle_change_symbol(self) -> dict:
+        self.game = self.game.change_symbol()
+        return {self.game.player1_username: self.game.player1_symbol,
+                self.game.player2_username: self.game.player2_symbol}
+
+    def change_symbol(self, event):
         self.send_json(event)
